7ac704deca5169ccda5bdf1d06ea10f8da6e4349
Kevin Purnelle <kevin.purnelle@gmail.com>
2018-07-31 12:43:26 +0200

    imp(images): defer load of some images

--- src/index.js
+++ src/index.js
@@ -1,1 +1,10 @@
 require('./less/index.less')
+
+function loadDeferredImages () {
+  const deferredImages = document.querySelectorAll('img[data-src]')
+  deferredImages.forEach(function (image) {
+    image.src = image.dataset.src
+  })
+}
+
+setTimeout(loadDeferredImages, 100)
